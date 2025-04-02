eda6e9f474ce18810e206aab591f538207385e83
Victor Ryan <vitorsilva3612@gmail.com>
2025-04-02 19:49:55 -0300

    feat: Home view

---
+++ pokedex_vue3/src/views/Home.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { onMounted } from 'vue';
+import { usePokemonStore } from '@/store/pokemonStore';
+
+const pokemonStore = usePokemonStore();
+
+onMounted(() => {
+  console.log('A')
+  pokemonStore.fetchPokemonList();
+});
+</script>
+
+<template>
+  <div class="container mx-auto p-4">
+    <h1 class="text-3xl font-bold text-center mb-6">Pokédex</h1>
+    <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+      <div
+        v-for="(pokemon, index) in pokemonStore.pokemonList"
+        :key="index"
+        class="p-4 border rounded shadow bg-white text-center"
+      >
+        <p class="text-lg font-semibold capitalize">{{ pokemon.name }}</p>
+      </div>
+    </div>
+  </div>
+</template>
